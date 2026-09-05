149fddc7bd1b1ebdfddbc93326df43bb1c99c7da
xiety <lesegomoima5@gmail.com>
2026-09-06 00:40:31 +0200

    chore(observability): Sentry wizard setup (SDK, instrumentation, source maps)

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://644a0f23075332f14186bcd6f900f572@o4511508267728896.ingest.us.sentry.io/4512036081303552",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
